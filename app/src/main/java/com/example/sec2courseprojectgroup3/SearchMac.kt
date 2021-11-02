5dbe0db6c5241af55947d4c5344441bc523b89b1
malmsteadaa <malmsteadaa@gmail.com>
2021-11-02 14:18:29 -0700

    Setting up the search function for mac addrrss lock up

--- app/src/main/java/com/example/sec2courseprojectgroup3/SearchMac.kt
+++ app/src/main/java/com/example/sec2courseprojectgroup3/SearchMac.kt
@@ -2,7 +2,7 @@ package com.example.sec2courseprojectgroup3
 import java.net.URL
 import java.sql.DriverManager.println
 
-fun lookupVendor(mac: String) = URL("http://api.macvendors.com/" + mac)
+fun lookupVendor(mac: String) = URL("http://api.macvendors.com/$mac")
 
 
 class SearchMac {
