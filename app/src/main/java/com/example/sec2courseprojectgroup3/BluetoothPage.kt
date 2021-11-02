45b0ecf18273131bc2c46cc4be07193aa287a577
malmsteadaa <malmsteadaa@gmail.com>
2021-11-02 12:35:54 -0700

    Setting up Activities for wifi and bluetooth

---
+++ app/src/main/java/com/example/sec2courseprojectgroup3/BluetoothPage.kt
@@ -0,0 +1,14 @@
+package com.example.sec2courseprojectgroup3
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class BluetoothPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bluetooth_page)
+
+
+    }
+}
